fe58e2997c8c6e686d290bb164de7b0820f0ed63
Emiya <Emiya@EmiyadeMacBook-Pro.local>
2017-11-22 17:50:35 +0800

    no message

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^u8yauup%3uh$t!xxh=$+s_($o_g(93bz7i+z&w_z#xzwu^on2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -121,3 +121,5 @@ USE_TZ = True
 
 # 素材路径
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
