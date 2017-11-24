7830c0a813aeaff20d1622ba64ae0f79e349adef
Emiya <Emiya@EmiyadeMacBook-Pro.local>
2017-11-24 11:28:32 +0800

    no message

--- BlogProject/settings.py
+++ BlogProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^u8yauup%3uh$t!xxh=$+s_($o_g(93bz7i+z&w_z#xzwu^on2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
