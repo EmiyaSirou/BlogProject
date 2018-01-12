81dea05e7de882573c843c6cd227238c99426d5f
Emiya <Emiya@EmiyadeMacBook-Pro.local>
2018-01-12 18:20:51 +0800

    no message

--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,26 @@
+import markdown
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.utils.html import strip_tags
 
-# Create your models here.
 
-#分类
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
 
-#标签
+
+# 标签
 class Tag(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
 
-#正文
+
+# 正文
 class Article(models.Model):
     title = models.CharField(max_length=30)
     body = models.TextField()
@@ -34,8 +36,24 @@ class Article(models.Model):
         return self.title + "\n" + self.body + "\n"
 
     def get_absolute_url(self):
-        return reverse('blog:detail', kwargs={'pk':self.pk})
+        return reverse('blog:detail', kwargs={'pk': self.pk})
 
     def increase_reading_quantity(self):
         self.reading_quantity += 1
         self.save(update_fields=['reading_quantity'])
+
+    def save(self, *args, **kwargs):
+        # 如果没有填写摘要
+        if not self.excerpt:
+            # 首先实例化一个 Markdown 类，用于渲染 body 的文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            # 先将 Markdown 文本渲染成 HTML 文本
+            # strip_tags 去掉 HTML 文本的全部 HTML 标签
+            # 从文本摘取前 54 个字符赋给 excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        # 调用父类的 save 方法将数据保存到数据库中
+        super(Article, self).save(*args, **kwargs)
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic import ListView, DetailView
+
 from blog.models import Article, Category
 from comments.forms import CommentForm
 from django.http import HttpResponse
@@ -38,3 +40,63 @@ def category(request, pk):
     # article_list = get_list_or_404(Article, category=tmp)
     article_list = Article.objects.filter(category=category).order_by('-creat_time')
     return render(request, 'blog/index.html', context={'article_list': article_list})
+
+
+class IndexView(ListView):
+    model = Article
+    template_name = 'blog/index.html'
+    context_object_name = 'article_list'
+    paginate_by = 10
+
+
+class CategoryView(ListView):
+    model = Article
+    template_name = 'blog/index.html'
+    context_object_name = 'article_list'
+
+    def get_queryset(self):
+        cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+        return super(CategoryView, self).get_queryset().filter(category=cate)
+
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Article
+    template_name = 'blog/detail.html'
+    context_object_name = 'article'
+
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_reading_quantity()
+
+        # 视图必须返回一个 HttpResponse 对象
+        return response
+
+    def get_object(self, queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post = super(PostDetailView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                          'markdown.extensions.extra',
+                                          'markdown.extensions.codehilite',
+                                          'markdown.extensions.toc',
+                                      ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        # 覆写 get_context_data 的目的是因为除了将 post 传递给模板外（DetailView 已经帮我们完成），
+        # 还要把评论表单、post 下的评论列表传递给模板。
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'comment_form': form,
+            'comment_list': comment_list
+        })
+        return context
